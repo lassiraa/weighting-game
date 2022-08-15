fa17206b28914293ce0868f52f18c4cdd05ec8d9
Lassi Raatikainen <raatikainenlassi@gmail.com>
2022-08-15 09:59:45 +0300

    add script to create 3d videos

---
+++ scripts/create_3d_videos.sh
@@ -0,0 +1,6 @@
+# Directory of validation images and output for new videos 
+in="CHANGE"  # e.g. ./coco/images/val2017/
+out="CHANGE"  # e.g. ./3d-videos/ remember to create the folder
+
+cd ../3d-ken-burns/
+python autozoom.py --in ${in} --out ${out}
