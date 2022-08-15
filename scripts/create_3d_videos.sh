fa157ef0e7835e4949a9a2008b18ea393806ba4a
Lassi Raatikainen <raatikainenlassi@gmail.com>
2022-08-15 10:13:55 +0300

    fix minor issue

--- scripts/create_3d_videos.sh
+++ scripts/create_3d_videos.sh
@@ -2,5 +2,5 @@
 in="CHANGE"  # e.g. ./coco/images/val2017/
 out="CHANGE"  # e.g. ./3d-videos/ remember to create the folder
 
-cd ../3d-ken-burns/
+cd ./3d-ken-burns/
 python autozoom.py --in ${in} --out ${out}
